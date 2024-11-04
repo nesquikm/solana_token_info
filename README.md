bf010a360f44e4ce1de4b692c9f4bf06721e7383
nesquikm <drapolyuk@gmail.com>
2024-11-04 19:11:19 +0400

    feat: add token-2022 metadata support

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-## Solana token info CLI client
+# Solana token info CLI client
 
 Simple CLI client to get information about Solana tokens.
 
-### Basic usage
+Supports fetching token metadata from Token-2022 and Metaplex Token Metadata.
+
+## Basic usage
 
 ```bash
 Usage: solana_token_info [OPTIONS] --address <ADDRESS>
@@ -15,10 +17,27 @@ Options:
   -V, --version              Print version
 ```
 
-### Sample output
+## Sample output
+
+With token metadata from Token-2022:
 
 ```bash
-➜  solana_token_info cargo run -- -a Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB  -e mainnet
+cargo run -- -a DaoQK21HaBxJ4t8s8r5HWtF6xBKVqdrKDeGzH8QjwJcp  -e devnet
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.24s
+     Running `target/debug/solana_token_info -a DaoQK21HaBxJ4t8s8r5HWtF6xBKVqdrKDeGzH8QjwJcp -e devnet`
+Token info:
+   * Name: thenes
+   * Symbol: THENES
+   * Supply 42
+   * URI: http://thenes.xyz
+   * IPv4 records: 1
+   * IPv6 records: 0
+```
+
+With token metadata from Metaplex Token Metadata:
+
+```bash
+cargo run -- -a Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB  -e mainnet
     Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.24s
      Running `target/debug/solana_token_info -a Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB -e mainnet`
 Token info:
@@ -41,8 +60,10 @@ cargo run -- -dd -a Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB  -e mainnet
 [*] Endpoint: mainnet
 [*] Requesting data from: mainnet (https://api.mainnet-beta.solana.com)
 [*] Pubkey parsed: Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB
+[*] Token metadata not found in SPL token account, trying to fetch from metadata account
+[*] Token metadata found in metadata account
 [*] Token metadata: TokenMetadata { name: "USDT", symbol: "USDT", uri: "" }
-[*] Token supply: 1889937984.92189
+[*] Token supply: 1889937981.94161
 [*] Token metadata URI is empty, trying to fetch from tokenlist
 [*] Token website from tokenlist: "https://tether.to/"
 [*] Fetching DNS records for website: https://tether.to/
@@ -51,7 +72,7 @@ cargo run -- -dd -a Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB  -e mainnet
 Token info:
    * Name: USDT
    * Symbol: USDT
-   * Supply 1889937984.92189
+   * Supply 1889937981.94161
    * URI: https://tether.to/
    * IPv4 records: 3
    * IPv6 records: 3
--- src/request/metadata.rs
+++ src/request/metadata.rs
@@ -1,5 +1,8 @@
 use anyhow::{anyhow, Ok, Result};
+use log::info;
 use mpl_token_metadata::accounts::Metadata;
+use solana_account_decoder::parse_token::{parse_token, TokenAccountType};
+use solana_account_decoder::parse_token_extension::{UiExtension, UiTokenMetadata};
 use solana_client::nonblocking::rpc_client::RpcClient;
 use solana_program::pubkey::Pubkey;
 
@@ -11,6 +14,59 @@ pub struct TokenMetadata {
 }
 
 pub async fn get_token_metadata(rpc: &RpcClient, pubkey: &Pubkey) -> Result<TokenMetadata> {
+    let account_data = rpc
+        .get_account_data(&pubkey)
+        .await
+        .map_err(|e| anyhow!("Error fetching account data: {}", e))?;
+
+    let token =
+        parse_token(&account_data, None).map_err(|e| anyhow!("Error parsing token: {}", e))?;
+
+    // Getting token metadata from SPL token account (Token-2022)
+    match token {
+        TokenAccountType::Mint(mint) => {
+            let metadata = mint.extensions.iter().find(|ext| {
+                if let UiExtension::TokenMetadata(UiTokenMetadata {
+                    update_authority: _,
+                    mint: _,
+                    name: _,
+                    symbol: _,
+                    uri: _,
+                    additional_metadata: _,
+                }) = ext
+                {
+                    return true;
+                }
+                return false;
+            });
+
+            match metadata {
+                Some(UiExtension::TokenMetadata(UiTokenMetadata {
+                    update_authority: _,
+                    mint: _,
+                    name,
+                    symbol,
+                    uri,
+                    additional_metadata: _,
+                })) => {
+                    info!("Token metadata found in SPL token account");
+                    return Ok(TokenMetadata {
+                        name: name.trim_end_matches('\0').to_owned(),
+                        symbol: symbol.trim_end_matches('\0').to_owned(),
+                        uri: uri.trim_end_matches('\0').to_owned(),
+                    });
+                }
+                _ => {}
+            }
+        }
+        _ => {}
+    }
+
+    info!("Token metadata not found in SPL token account, trying to fetch from metadata account");
+
+    // Okay, we don't have metadata in the SPL token account, let's try to fetch it from the
+    // metadata account (Metaplex Token Metadata)
+
     let (metadata_pda, _) = Metadata::find_pda(&pubkey);
 
     let metadata_data = rpc
@@ -20,6 +76,8 @@ pub async fn get_token_metadata(rpc: &RpcClient, pubkey: &Pubkey) -> Result<Toke
 
     let metadata = mpl_token_metadata::accounts::Metadata::safe_deserialize(&metadata_data)
         .map_err(|e| anyhow!("Error deserializing metadata: {}", e))?;
+
+    info!("Token metadata found in metadata account");
 
     Ok(TokenMetadata {
         name: metadata.name.trim_end_matches('\0').to_owned(),
